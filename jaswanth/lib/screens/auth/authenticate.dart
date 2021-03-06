67929f7f42bb7621365b4252c9b95afd781dc9eb
jaswanth chilaka <chilaka.jaswanth@gmail.com>
2021-03-06 12:22:58 +0530

    added screens folder for flexiblity

---
+++ jaswanth/lib/screens/auth/authenticate.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class authenticate extends StatefulWidget {
+  @override
+  _authenticateState createState() => _authenticateState();
+}
+
+class _authenticateState extends State<authenticate> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Authenticate')
+    );
+  }
+}
